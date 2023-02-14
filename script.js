727be4668865d4890afb3e17ce283995b0f62f41
Suraj-Accio <124239927+Suraj-Accio@users.noreply.github.com>
2023-02-14 17:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
-function palindrome(str){
-
-}
+const palindromeChecker = (str) => {
+  let temp = str.toLowerCase();
+  let left = 0
+  let right = str.length()-1
+  while(left < right) {
+	  let char_left = str.charAt(left)
+	  let char_right = str.charAt(right)
+	  if(char_left != char_right) {
+		  return false;
+	  }
+  }
+	return true
+};
 module.exports = palindrome
